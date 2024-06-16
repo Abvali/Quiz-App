bcebd3c44fd35dc9f2869d6b0b6fdae39c8400f6
Maryam Abvali <Abvali.maryam@gmail.com>
2024-06-16 09:52:04 +0200

    guter stand

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 
-const bookmarkElement = document.querySelectorAll ('[data-js="bookmark"]');
-console.dir(bookmarkElement)
 
-bookmarkElement.forEach(element => {
+
+const bookmarkElements = document.querySelectorAll ('[data-js="bookmark"]');
+
+
+bookmarkElements.forEach(element => {
     element.addEventListener ('click', () => {
       
         element.classList.toggle('bookmark-select')
@@ -10,3 +12,35 @@ bookmarkElement.forEach(element => {
     });
 });
 
+const answerButtons = document.querySelectorAll(`[data-js="answer-btn"]`);
+console.log(answerButtons)
+
+answerButtons.forEach(button => {
+
+    button.addEventListener("click" , ()=> {
+        if (button.innerHTML === "Show Answer") {
+            button.innerHTML = "Hide Answer"
+        }else{
+            button.innerHTML = "Show Answer"
+        }    
+        
+        const showAnswer = button.nextElementSibling;
+        showAnswer.classList.toggle("showanswer")
+
+    });
+
+})
+
+
+const darkModeBtn = document.querySelector(`[data-js="dark-mode"]`);
+console.log(darkModeBtn)
+
+if(darkModeBtn != null){
+    darkModeBtn.addEventListener("click" , ()=> {
+        darkModeBtn.classList.toggle("active");
+        const body = document.body;
+        // body.classList.toggle("light-theme")
+        body.classList.toggle("darkmode-theme")
+
+    });
+}
